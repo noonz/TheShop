03ccd5b0c1aa57820de3d5032a62e9c2b04ba873
dave nunez <noonz2@icloud.com>
2022-08-20 00:53:18 -0400

    add missing folders

---
+++ src/app/components/pages/products/products.component.ts
@@ -0,0 +1,57 @@
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { map, Subscription } from 'rxjs';
+import { Product } from 'src/app/models/products';
+import { ProductService } from 'src/app/services/product.service';
+
+@Component({
+    selector: 'app-products',
+    templateUrl: './products.component.html',
+    styleUrls: ['./products.component.scss']
+})
+export class ProductsComponent implements OnInit, OnDestroy {
+    products: Product[] = [];
+    filteredProducts!: Product[];
+    subscription!: Subscription;
+    category!: string;
+
+    constructor(
+        private productService: ProductService,
+        private route: ActivatedRoute
+    ) {}
+
+    ngOnInit(): void {
+        this.subscription = this.productService
+            .getAll()
+            .pipe(
+                map((changes) =>
+                    changes.map((c) => {
+                        return {
+                            key: c.payload.key!,
+                            ...(c.payload.val() as Product)
+                        };
+                    })
+                )
+            )
+            .subscribe(
+                (products) => (
+                    (this.products = products),
+                    this.route.queryParamMap.subscribe((params) => {
+                        this.category = params.get('category')!;
+                        this.filteredProducts = this.category
+                            ? this.products.filter(
+                                  (p) => p.category === this.category
+                              )
+                            : this.products;
+                    })
+                )
+            );
+
+    }
+
+    ngOnDestroy(): void {
+        if (this.subscription) {
+            this.subscription.unsubscribe();
+        }
+    }
+}
